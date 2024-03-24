03e3e7fe40b92f83d40934af355238799d7a7ff5
Akane Kano <bglakane@gmail.com>
2024-03-24 19:38:41 +0900

    displaying comments

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true}))
+const path = require('path');
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
- app.get('/tacos', (req, res)=>{
+app.get('/tacos', (req, res) => {
   res.send("GET /tacos response")
- })
-app.post('/tacos', (req, res)=>{
+})
+
+
+const comments = [
+  {
+
+      username: 'Todd',
+      comment: 'lol that is so funny!'
+  },
+  {
+
+      username: 'Skyler',
+      comment: 'I like to go birdwatching with my dog'
+  },
+  {
+
+      username: 'Sk8erBoi',
+      comment: 'Plz delete your account, Todd'
+  },
+  {
+
+      username: 'onlysayswoof',
+      comment: 'woof woof woof'
+  }
+];
+
+app.get('/comments', (req, res) => {
+  res.render('comments/index', {comments})
+});
+
+app.post('/tacos', (req, res) => {
   const { meat, qty } = req.body;
   console.log(req.body)
   res.send(`OK, here are your ${qty} ${meat} tacos`)
- })
+})
 
- app.listen(3000, () => {
+app.listen(3000, () => {
   console.log("ON PORT 3000")
- })
+})
+
 
