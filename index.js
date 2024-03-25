54406369663adb48f89470b2785636427584a637
Akane Kano <bglakane@gmail.com>
2024-03-25 13:29:39 +0900

    adding uuid

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -17,22 +19,22 @@ Create, Read, Update, Delete and Destroy
 
 const comments = [
   {
-
+      id: uuid(),
       username: 'Todd',
       comment: 'lol that is so funny!'
   },
   {
-
+      id: uuid(),
       username: 'Skyler',
       comment: 'I like to go birdwatching with my dog'
   },
   {
-
+      id: uuid(),
       username: 'Sk8erBoi',
       comment: 'Plz delete your account, Todd'
   },
   {
-
+      id: uuid(),
       username: 'onlysayswoof',
       comment: 'woof woof woof'
   }
@@ -49,10 +51,14 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
-  comments.push({username,comment});
-  res.send("IT WORKED!")
+  comments.push({username,comment, id:uuid()});
+  res.redirect('/comments');
 })
-
+app.get('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === (id));
+  res.render('comments/show', { comment })
+})
 app.post('/tacos', (req, res) => {
   const { meat, qty } = req.body;
   console.log(req.body)
